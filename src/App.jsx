c94c27ddacdb0c7868eaab56eef190bc7ed529b4
srisaisnigdha <98080072+srisaisnigdha@users.noreply.github.com>
2024-02-22 23:33:44 +0530

    added TicTacToe game title and included Roboto font

--- src/App.jsx
+++ src/App.jsx
@@ -77,14 +77,15 @@ function App() {
 
    return (
       <div className='app'>
+         <h1>TIC <span className='text-green'>TAC</span> TOE</h1>
          {/* <StatusMessage winner={winner} isXNext={isXNext} squares={squares} /> */}
          <StatusMessage winner={winner} gamingBoard={gamingBoard} />
          <Board squares={gamingBoard.squares} handleSquareClick={handleSquareClick} winningSquares={winningSquares} />
 
          <button type='button' className={`btn-reset ${winner ? 'active' : ''}`} onClick={onNewGameStart}>Start New Game</button>
-         <h2>Current Game History</h2>
+         <h2 style={{ fontWeight: 'normal', }}>Current Game History</h2>
          <History history={history} moveTo={moveTo} currentMove={currentMove} />
-      </div>
+      </div >
    );
 }
 
